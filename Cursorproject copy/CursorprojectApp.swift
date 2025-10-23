e83dc73aa975e4541a7eaf659c1fdb67328daf7b
Eyesaac0 <mrisaacjdunn@gmail.com>
2025-10-23 18:08:42 -0500

    calculator app commit

---
+++ Cursorproject copy/CursorprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorprojectApp.swift
+//  Cursorproject
+//
+//  Created by 35 GO Participant on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CursorprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
